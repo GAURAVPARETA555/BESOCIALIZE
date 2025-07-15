b086e3656454af3ac22d25534fdbbdf4a250e17f
GAURAVPARETA555 <gauravpareta18@gmail.com>
2025-07-15 20:08:19 +0530

    Updated settings and collected static files

--- BESocialize/settings.py
+++ BESocialize/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-92m&zpg41*no0&6-50^p3+)w-ryruwyi$2vwr)jwj6mzsux@l('
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-default-for-dev')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("RENDER_EXTERNAL_HOSTNAME", "").split(",")
+
 
 
 # Application definition
@@ -120,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
