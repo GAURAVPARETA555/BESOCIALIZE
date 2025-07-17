3cf5c724d3905bc5e5e24caf64dd68b63f843763
GAURAVPARETA555 <gauravpareta18@gmail.com>
2025-07-17 15:37:31 +0530

    Added Render domain to ALLOWED_HOST

--- BESocialize/settings.py
+++ BESocialize/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-default-for-dev')
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'besocialize-1.onrender.com']
+
 
 
 
